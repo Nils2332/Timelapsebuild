1ff94cedbe4d9f9f0876ba386c7cf12b34b0b0d8
Nils2332 <nils2332@googlemail.com>
2019-09-16 16:34:59 +0200

    update

---
+++ README.md
@@ -0,0 +1,20 @@
+
+# 3 Axis Camera Motion System
+
+A simple 4 digit stopwatch triggered by 2 remote Buttons<br />
+
+![Overview](https://github.com/Nils2332/Timelapsebuild/tree/master/Pictures/Slider-Example.gif)<br />
+Demo of Moving System<br />
+![Main Body](https://github.com/Nils2332/Timelapsebuild/tree/master/Pictures/Motion-System.jpg)<br />
+Motion-System<br />
+![Animation Demo](https://github.com/Nils2332/Timelapsebuild/tree/master/Pictures/Remote-Controller.jpg)<br />
+Remote-Controller<br />
+## Built With
+Motor Driver Board with STM32 Microcontroller and TMC2130 Stepper-Driver.<br />
+Remote-Controller with STM32 $\muC
+$ Touchscreen and two Joysticks.<br />
+Both connected remotly via nRF905 wireless Chip on 868 MHz band. <br />
+
+## License
+
+This project is licensed under the MIT License
--- Software/Remote-Control/Debug/Src/stm32f4xx_it.d
+++ Software/Remote-Control/Debug/Src/stm32f4xx_it.d
@@ -1,103 +1,103 @@
 Src/stm32f4xx_it.o: ../Src/stm32f4xx_it.c \
- C:/Users/Nils/STMWorkbench/workspace/Timelapsebuild_2.1/Inc/main.h \
- C:/Users/Nils/STMWorkbench/workspace/Timelapsebuild_2.1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h \
- C:/Users/Nils/STMWorkbench/workspace/Timelapsebuild_2.1/Inc/stm32f4xx_hal_conf.h \
- C:/Users/Nils/STMWorkbench/workspace/Timelapsebuild_2.1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h \
- C:/Users/Nils/STMWorkbench/workspace/Timelapsebuild_2.1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h \
- C:/Users/Nils/STMWorkbench/workspace/Timelapsebuild_2.1/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
- C:/Users/Nils/STMWorkbench/workspace/Timelapsebuild_2.1/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f401xe.h \
- C:/Users/Nils/STMWorkbench/workspace/Timelapsebuild_2.1/Drivers/CMSIS/Include/core_cm4.h \
- C:/Users/Nils/STMWorkbench/workspace/Timelapsebuild_2.1/Drivers/CMSIS/Include/core_cmInstr.h \
- C:/Users/Nils/STMWorkbench/workspace/Timelapsebuild_2.1/Drivers/CMSIS/Include/cmsis_gcc.h \
- C:/Users/Nils/STMWorkbench/workspace/Timelapsebuild_2.1/Drivers/CMSIS/Include/core_cmFunc.h \
- C:/Users/Nils/STMWorkbench/workspace/Timelapsebuild_2.1/Drivers/CMSIS/Include/core_cmSimd.h \
- C:/Users/Nils/STMWorkbench/workspace/Timelapsebuild_2.1/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
- C:/Users/Nils/STMWorkbench/workspace/Timelapsebuild_2.1/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
- C:/Users/Nils/STMWorkbench/workspace/Timelapsebuild_2.1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h \
- C:/Users/Nils/STMWorkbench/workspace/Timelapsebuild_2.1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h \
- C:/Users/Nils/STMWorkbench/workspace/Timelapsebuild_2.1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h \
- C:/Users/Nils/STMWorkbench/workspace/Timelapsebuild_2.1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h \
- C:/Users/Nils/STMWorkbench/workspace/Timelapsebuild_2.1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h \
- C:/Users/Nils/STMWorkbench/workspace/Timelapsebuild_2.1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h \
- C:/Users/Nils/STMWorkbench/workspace/Timelapsebuild_2.1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_adc.h \
- C:/Users/Nils/STMWorkbench/workspace/Timelapsebuild_2.1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_adc_ex.h \
- C:/Users/Nils/STMWorkbench/workspace/Timelapsebuild_2.1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h \
- C:/Users/Nils/STMWorkbench/workspace/Timelapsebuild_2.1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h \
- C:/Users/Nils/STMWorkbench/workspace/Timelapsebuild_2.1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h \
- C:/Users/Nils/STMWorkbench/workspace/Timelapsebuild_2.1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h \
- C:/Users/Nils/STMWorkbench/workspace/Timelapsebuild_2.1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h \
- C:/Users/Nils/STMWorkbench/workspace/Timelapsebuild_2.1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_spi.h \
- C:/Users/Nils/STMWorkbench/workspace/Timelapsebuild_2.1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h \
- C:/Users/Nils/STMWorkbench/workspace/Timelapsebuild_2.1/Inc/stm32f4xx_it.h \
- C:/Users/Nils/STMWorkbench/workspace/Timelapsebuild_2.1/App/App.h \
- C:/Users/Nils/STMWorkbench/workspace/Timelapsebuild_2.1/Inc/usart.h \
- C:/Users/Nils/STMWorkbench/workspace/Timelapsebuild_2.1/Inc/main.h \
- C:/Users/Nils/STMWorkbench/workspace/Timelapsebuild_2.1/Inc/gpio.h
+ C:/Users/Nils/Documents/Projects/TimelapsBuild\ V2/Software/Remote-Control/Inc/main.h \
+ C:/Users/Nils/Documents/Projects/TimelapsBuild\ V2/Software/Remote-Control/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h \
+ C:/Users/Nils/Documents/Projects/TimelapsBuild\ V2/Software/Remote-Control/Inc/stm32f4xx_hal_conf.h \
+ C:/Users/Nils/Documents/Projects/TimelapsBuild\ V2/Software/Remote-Control/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h \
+ C:/Users/Nils/Documents/Projects/TimelapsBuild\ V2/Software/Remote-Control/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h \
+ C:/Users/Nils/Documents/Projects/TimelapsBuild\ V2/Software/Remote-Control/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ C:/Users/Nils/Documents/Projects/TimelapsBuild\ V2/Software/Remote-Control/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f401xe.h \
+ C:/Users/Nils/Documents/Projects/TimelapsBuild\ V2/Software/Remote-Control/Drivers/CMSIS/Include/core_cm4.h \
+ C:/Users/Nils/Documents/Projects/TimelapsBuild\ V2/Software/Remote-Control/Drivers/CMSIS/Include/core_cmInstr.h \
+ C:/Users/Nils/Documents/Projects/TimelapsBuild\ V2/Software/Remote-Control/Drivers/CMSIS/Include/cmsis_gcc.h \
+ C:/Users/Nils/Documents/Projects/TimelapsBuild\ V2/Software/Remote-Control/Drivers/CMSIS/Include/core_cmFunc.h \
+ C:/Users/Nils/Documents/Projects/TimelapsBuild\ V2/Software/Remote-Control/Drivers/CMSIS/Include/core_cmSimd.h \
+ C:/Users/Nils/Documents/Projects/TimelapsBuild\ V2/Software/Remote-Control/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ C:/Users/Nils/Documents/Projects/TimelapsBuild\ V2/Software/Remote-Control/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ C:/Users/Nils/Documents/Projects/TimelapsBuild\ V2/Software/Remote-Control/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h \
+ C:/Users/Nils/Documents/Projects/TimelapsBuild\ V2/Software/Remote-Control/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h \
+ C:/Users/Nils/Documents/Projects/TimelapsBuild\ V2/Software/Remote-Control/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h \
+ C:/Users/Nils/Documents/Projects/TimelapsBuild\ V2/Software/Remote-Control/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h \
+ C:/Users/Nils/Documents/Projects/TimelapsBuild\ V2/Software/Remote-Control/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h \
+ C:/Users/Nils/Documents/Projects/TimelapsBuild\ V2/Software/Remote-Control/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h \
+ C:/Users/Nils/Documents/Projects/TimelapsBuild\ V2/Software/Remote-Control/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_adc.h \
+ C:/Users/Nils/Documents/Projects/TimelapsBuild\ V2/Software/Remote-Control/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_adc_ex.h \
+ C:/Users/Nils/Documents/Projects/TimelapsBuild\ V2/Software/Remote-Control/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h \
+ C:/Users/Nils/Documents/Projects/TimelapsBuild\ V2/Software/Remote-Control/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h \
+ C:/Users/Nils/Documents/Projects/TimelapsBuild\ V2/Software/Remote-Control/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h \
+ C:/Users/Nils/Documents/Projects/TimelapsBuild\ V2/Software/Remote-Control/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h \
+ C:/Users/Nils/Documents/Projects/TimelapsBuild\ V2/Software/Remote-Control/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h \
+ C:/Users/Nils/Documents/Projects/TimelapsBuild\ V2/Software/Remote-Control/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_spi.h \
+ C:/Users/Nils/Documents/Projects/TimelapsBuild\ V2/Software/Remote-Control/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h \
+ C:/Users/Nils/Documents/Projects/TimelapsBuild\ V2/Software/Remote-Control/Inc/stm32f4xx_it.h \
+ C:/Users/Nils/Documents/Projects/TimelapsBuild\ V2/Software/Remote-Control/App/App.h \
+ C:/Users/Nils/Documents/Projects/TimelapsBuild\ V2/Software/Remote-Control/Inc/usart.h \
+ C:/Users/Nils/Documents/Projects/TimelapsBuild\ V2/Software/Remote-Control/Inc/main.h \
+ C:/Users/Nils/Documents/Projects/TimelapsBuild\ V2/Software/Remote-Control/Inc/gpio.h
 
-C:/Users/Nils/STMWorkbench/workspace/Timelapsebuild_2.1/Inc/main.h:
+C:/Users/Nils/Documents/Projects/TimelapsBuild\ V2/Software/Remote-Control/Inc/main.h:
 
-C:/Users/Nils/STMWorkbench/workspace/Timelapsebuild_2.1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h:
+C:/Users/Nils/Documents/Projects/TimelapsBuild\ V2/Software/Remote-Control/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h:
 
-C:/Users/Nils/STMWorkbench/workspace/Timelapsebuild_2.1/Inc/stm32f4xx_hal_conf.h:
+C:/Users/Nils/Documents/Projects/TimelapsBuild\ V2/Software/Remote-Control/Inc/stm32f4xx_hal_conf.h:
 
-C:/Users/Nils/STMWorkbench/workspace/Timelapsebuild_2.1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h:
+C:/Users/Nils/Documents/Projects/TimelapsBuild\ V2/Software/Remote-Control/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h:
 
-C:/Users/Nils/STMWorkbench/workspace/Timelapsebuild_2.1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h:
+C:/Users/Nils/Documents/Projects/TimelapsBuild\ V2/Software/Remote-Control/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h:
 
-C:/Users/Nils/STMWorkbench/workspace/Timelapsebuild_2.1/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+C:/Users/Nils/Documents/Projects/TimelapsBuild\ V2/Software/Remote-Control/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
 
-C:/Users/Nils/STMWorkbench/workspace/Timelapsebuild_2.1/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f401xe.h:
+C:/Users/Nils/Documents/Projects/TimelapsBuild\ V2/Software/Remote-Control/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f401xe.h:
 
-C:/Users/Nils/STMWorkbench/workspace/Timelapsebuild_2.1/Drivers/CMSIS/Include/core_cm4.h:
+C:/Users/Nils/Documents/Projects/TimelapsBuild\ V2/Software/Remote-Control/Drivers/CMSIS/Include/core_cm4.h:
 
-C:/Users/Nils/STMWorkbench/workspace/Timelapsebuild_2.1/Drivers/CMSIS/Include/core_cmInstr.h:
+C:/Users/Nils/Documents/Projects/TimelapsBuild\ V2/Software/Remote-Control/Drivers/CMSIS/Include/core_cmInstr.h:
 
-C:/Users/Nils/STMWorkbench/workspace/Timelapsebuild_2.1/Drivers/CMSIS/Include/cmsis_gcc.h:
+C:/Users/Nils/Documents/Projects/TimelapsBuild\ V2/Software/Remote-Control/Drivers/CMSIS/Include/cmsis_gcc.h:
 
-C:/Users/Nils/STMWorkbench/workspace/Timelapsebuild_2.1/Drivers/CMSIS/Include/core_cmFunc.h:
+C:/Users/Nils/Documents/Projects/TimelapsBuild\ V2/Software/Remote-Control/Drivers/CMSIS/Include/core_cmFunc.h:
 
-C:/Users/Nils/STMWorkbench/workspace/Timelapsebuild_2.1/Drivers/CMSIS/Include/core_cmSimd.h:
+C:/Users/Nils/Documents/Projects/TimelapsBuild\ V2/Software/Remote-Control/Drivers/CMSIS/Include/core_cmSimd.h:
 
-C:/Users/Nils/STMWorkbench/workspace/Timelapsebuild_2.1/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+C:/Users/Nils/Documents/Projects/TimelapsBuild\ V2/Software/Remote-Control/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
 
-C:/Users/Nils/STMWorkbench/workspace/Timelapsebuild_2.1/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+C:/Users/Nils/Documents/Projects/TimelapsBuild\ V2/Software/Remote-Control/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
 
-C:/Users/Nils/STMWorkbench/workspace/Timelapsebuild_2.1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h:
+C:/Users/Nils/Documents/Projects/TimelapsBuild\ V2/Software/Remote-Control/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h:
 
-C:/Users/Nils/STMWorkbench/workspace/Timelapsebuild_2.1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h:
+C:/Users/Nils/Documents/Projects/TimelapsBuild\ V2/Software/Remote-Control/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h:
 
-C:/Users/Nils/STMWorkbench/workspace/Timelapsebuild_2.1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h:
+C:/Users/Nils/Documents/Projects/TimelapsBuild\ V2/Software/Remote-Control/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h:
 
-C:/Users/Nils/STMWorkbench/workspace/Timelapsebuild_2.1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h:
+C:/Users/Nils/Documents/Projects/TimelapsBuild\ V2/Software/Remote-Control/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h:
 
-C:/Users/Nils/STMWorkbench/workspace/Timelapsebuild_2.1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h:
+C:/Users/Nils/Documents/Projects/TimelapsBuild\ V2/Software/Remote-Control/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h:
 
-C:/Users/Nils/STMWorkbench/workspace/Timelapsebuild_2.1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h:
+C:/Users/Nils/Documents/Projects/TimelapsBuild\ V2/Software/Remote-Control/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h:
 
-C:/Users/Nils/STMWorkbench/workspace/Timelapsebuild_2.1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_adc.h:
+C:/Users/Nils/Documents/Projects/TimelapsBuild\ V2/Software/Remote-Control/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_adc.h:
 
-C:/Users/Nils/STMWorkbench/workspace/Timelapsebuild_2.1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_adc_ex.h:
+C:/Users/Nils/Documents/Projects/TimelapsBuild\ V2/Software/Remote-Control/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_adc_ex.h:
 
-C:/Users/Nils/STMWorkbench/workspace/Timelapsebuild_2.1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h:
+C:/Users/Nils/Documents/Projects/TimelapsBuild\ V2/Software/Remote-Control/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h:
 
-C:/Users/Nils/STMWorkbench/workspace/Timelapsebuild_2.1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h:
+C:/Users/Nils/Documents/Projects/TimelapsBuild\ V2/Software/Remote-Control/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h:
 
-C:/Users/Nils/STMWorkbench/workspace/Timelapsebuild_2.1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h:
+C:/Users/Nils/Documents/Projects/TimelapsBuild\ V2/Software/Remote-Control/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h:
 
-C:/Users/Nils/STMWorkbench/workspace/Timelapsebuild_2.1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h:
+C:/Users/Nils/Documents/Projects/TimelapsBuild\ V2/Software/Remote-Control/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h:
 
-C:/Users/Nils/STMWorkbench/workspace/Timelapsebuild_2.1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h:
+C:/Users/Nils/Documents/Projects/TimelapsBuild\ V2/Software/Remote-Control/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h:
 
-C:/Users/Nils/STMWorkbench/workspace/Timelapsebuild_2.1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_spi.h:
+C:/Users/Nils/Documents/Projects/TimelapsBuild\ V2/Software/Remote-Control/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_spi.h:
 
-C:/Users/Nils/STMWorkbench/workspace/Timelapsebuild_2.1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h:
+C:/Users/Nils/Documents/Projects/TimelapsBuild\ V2/Software/Remote-Control/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h:
 
-C:/Users/Nils/STMWorkbench/workspace/Timelapsebuild_2.1/Inc/stm32f4xx_it.h:
+C:/Users/Nils/Documents/Projects/TimelapsBuild\ V2/Software/Remote-Control/Inc/stm32f4xx_it.h:
 
-C:/Users/Nils/STMWorkbench/workspace/Timelapsebuild_2.1/App/App.h:
+C:/Users/Nils/Documents/Projects/TimelapsBuild\ V2/Software/Remote-Control/App/App.h:
 
-C:/Users/Nils/STMWorkbench/workspace/Timelapsebuild_2.1/Inc/usart.h:
+C:/Users/Nils/Documents/Projects/TimelapsBuild\ V2/Software/Remote-Control/Inc/usart.h:
 
-C:/Users/Nils/STMWorkbench/workspace/Timelapsebuild_2.1/Inc/main.h:
+C:/Users/Nils/Documents/Projects/TimelapsBuild\ V2/Software/Remote-Control/Inc/main.h:
 
-C:/Users/Nils/STMWorkbench/workspace/Timelapsebuild_2.1/Inc/gpio.h:
+C:/Users/Nils/Documents/Projects/TimelapsBuild\ V2/Software/Remote-Control/Inc/gpio.h:
